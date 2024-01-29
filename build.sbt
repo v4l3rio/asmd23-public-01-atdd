545b4cc050002b372129a7824340e222825e289a
mirko.viroli <mirko.viroli@unibo.it>
2024-01-29 17:01:54 +0100

    added some cucumber

--- build.sbt
+++ build.sbt
@@ -2,5 +2,7 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 lazy val root = (project in file("."))
   .settings(
     name := "java-sbt-example",
-    libraryDependencies += "net.aichler" % "jupiter-interface" % JupiterKeys.jupiterVersion.value % Test
+    libraryDependencies ++= Seq(
+        "net.aichler" % "jupiter-interface" % JupiterKeys.jupiterVersion.value % Test,
+        "io.cucumber" %% "cucumber-java" % "6.1.1" % Test)
 )
---
+++ src/test/java/MyStepdefs.java
@@ -0,0 +1,27 @@
+
+
+public class MyStepdefs {
+    @io.cucumber.java.en.Given("^I have a Calculator$")
+    public void iHaveACalculator() {
+    }
+
+    @io.cucumber.java.en.When("^I add (\\d+) and (\\d+)$")
+    public void iAddAnd(int arg0, int arg1) {
+    }
+
+    @io.cucumber.java.en.Then("^the sum should be (\\d+)$")
+    public void theSumShouldBe(int arg0) {
+    }
+
+    @io.cucumber.java.en.When("^I add (\\d+) and -(\\d+)$")
+    public void iAddAnd(int arg0, int arg1) {
+    }
+
+    @io.cucumber.java.en.When("^I add -(\\d+) and -(\\d+)$")
+    public void iAddAnd(int arg0, int arg1) {
+    }
+
+    @io.cucumber.java.en.Then("^the sum should be -(\\d+)$")
+    public void theSumShouldBe(int arg0) {
+    }
+}
